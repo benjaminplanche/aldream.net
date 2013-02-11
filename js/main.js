07496cbd1d2174dac5b17464648537c9aa3d9888
Aldream <benjamin.planche@aldream.net>
2013-02-11 16:29:31 +0900

    <Design> Header

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
 	$('#blog-masonry').masonry({
-	  itemSelector: 'article',
-	  // set columnWidth a fraction of the container width
-	  columnWidth: function( containerWidth ) {
-		return containerWidth / 3;
-	  },
-	  isFitWidth: true
+		itemSelector: 'article',
+		isFitWidth: true,
+		columnWidth: 320,
+		gutterWidth: 20
 	});
 });
