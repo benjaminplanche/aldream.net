ba942338999ae457f847bb661aad1573ae450013
Aldream <benjamin.planche@aldream.net>
2013-03-11 00:20:54 +0900

    <JS> Close ...

--- js/main.js
+++ js/main.js
@@ -3,110 +3,54 @@ $(document).ready(function(){
 		itemSelector: 'article',
 		gutterWidth: 20
 	});
-	$('#board').css('height', $(document).height()+'px');
+	$('> section').css('height', $(document).height()+'px');
 	
-	$('a').each(function() {
+	function Navigation() {
 		var target = $(this).attr('href');
 		if (target.indexOf("://") == -1) { // Intern link	
 			$(this).click(function () {
 				var newPage = target.split(".")[0], isContentLoaded = false, isDezoomOver = 1;
+				history.pushState(null, newPage, newPage+'.html');
 				if (!Aldream.cache[Aldream.currentPage]) { // We save the current page in the cache:
-					Aldream.cache[Aldream.currentPage] = {meta: {}, body: $('body').html()};
+					Aldream.cache[Aldream.currentPage] = {meta: {}, body: '<section>'+$('body > section').html()+'</section>'};
 				}
 				if (Aldream.doTransition) {
-					if (Aldream.cache['meta']) {
-						$('body').html(Aldream.cache['meta']);
-						$('#'+Aldream.currentPage).html(Aldream.cache[Aldream.currentPage].body);
-						Aldream.transformZoom(Aldream.currentPage); // Positioning every element relative to the current user's position.
-						Aldream.transformDezoom('all 1.5s', function(){alert('ok');}); // Then dezooming to see the whole picture.
-					}
-					else {
-						$('body').load('meta.html', function (rep) {
-							Aldream.cache['meta'] = {meta: {}, body: rep.split('<body>')[1].split('</body>')[0]};
-							$('#'+Aldream.currentPage).html(Aldream.cache[Aldream.currentPage].body);
-							Aldream.transformZoom(Aldream.currentPage); // Positioning every element relative to the current user's position.
-							
-							
-							// First we dezoom a bit and make the current content fade away to delete it (keeping the content would slow the transition):
-							// Then we move the camera back to its initial position, watching the whole scene:
+					$('body').append('<div style="visibility:hidden;" id="meta"><div id="meta-index"></div><div id="meta-blog"></div><div id="meta-demo"></div><div id="meta-projects"></div><div id="meta-about"></div></div>');
+					Aldream.transformZoom(Aldream.currentPage); // Positioning every element relative to the current user's position.
+					$('#meta-'+Aldream.currentPage).html(Aldream.cache[Aldream.currentPage].body);
+					$('#meta').css('visibility','visible');
+					$('body > section').remove();
+					$('body').removeAttr('id');
+					// First we dezoom a bit and make the current content fade away to delete it (keeping the content would slow the transition):
+					// Then we move the camera back to its initial position, watching the whole scene:
 					
-							for (var el in Aldream.metaPositions) {
-								$('#'+el)[0].style[Modernizr.prefixed('transition')] = 'transform 2s';
-								var tr = [];
-								for (var i = 0; i < 6; i++) {
-									tr[i] = Aldream.metaPositions[el][i]-Aldream.metaPositions[Aldream.currentPage][i]+Aldream.origOffset[Aldream.currentPage][i];
+					
+					setTimeout(function(){
+						$('#meta-'+ Aldream.currentPage + ' > section')[0].style[Modernizr.prefixed('transition')] = 'opacity 1s';
+						$('#meta-'+ Aldream.currentPage + ' > section').css('opacity', 0);
+					
+						for (var el in Aldream.metaPositions) {
+							$('#meta-'+el).unbind('transitionend webkitTransitionEnd oTransitionEnd otransitionend');
+							$('#meta-'+el).on('transitionend webkitTransitionEnd oTransitionEnd otransitionend', function() {
+								isDezoomOver++;
+								if (isContentLoaded && isDezoomOver == Aldream.nbPages) {
+									ZoomIn(newPage);
 								}
-								tr = 'translateX('+tr[0]+'em) translateY('+(tr[1])+'em) translateZ('+(tr[2])+'em) rotateX('+tr[3]+'deg) rotateY('+tr[4]+'deg) rotateZ('+tr[5]+'deg)';
-								$('#'+el)[0].style[Modernizr.prefixed('transform')] = tr; // Positioning every element relative to the current user's position.
-							}
+							});
 							
-							
-							$('#'+ Aldream.currentPage + ' #board')[0].style[Modernizr.prefixed('transition')] = 'opacity 1s';
-							$('#'+ Aldream.currentPage + ' #board').css('opacity', 0);
-							
-							for (var el in Aldream.metaPositions) {
-								$('#'+el).unbind('transitionend webkitTransitionEnd oTransitionEnd otransitionend');
-								$('#'+el).on('transitionend webkitTransitionEnd oTransitionEnd otransitionend', function() {
-									isDezoomOver++;
-									if (isContentLoaded && isDezoomOver == Aldream.nbPages) {
-										// We delete the previous content:
-										$('#'+Aldream.currentPage).unbind('transitionend webkitTransitionEnd oTransitionEnd otransitionend');
-										$('#'+Aldream.currentPage).html('');
-											
-										for (var elClo in Aldream.metaPositions) {
-											$('#'+elClo).unbind('transitionend webkitTransitionEnd oTransitionEnd otransitionend');
-
-											$('#'+elClo)[0].style[Modernizr.prefixed('transition')] = 'transform 2s';
-											var tr = [];
-											for (var i = 0; i < 6; i++) {
-												tr[i] = Aldream.metaPositions[elClo][i]-Aldream.metaPositions[newPage][i]+Aldream.origOffset[newPage][i];
-											}
-											tr = 'translateX('+tr[0]+'em) translateY('+(tr[1])+'em) translateZ('+(tr[2])+'em) rotateX('+tr[3]+'deg) rotateY('+tr[4]+'deg) rotateZ('+tr[5]+'deg)';
-											$('#'+elClo)[0].style[Modernizr.prefixed('transform')] = tr;
-										}
-										
-										$('#'+newPage).html(Aldream.cache[newPage].body);
-										$('#'+ newPage + ' #board').css('opacity', 0);
-										$('#'+ newPage + ' #board')[0].style[Modernizr.prefixed('transition')] = 'opacity 20s 1.5s';
-										$('#'+ newPage + ' #board').css('opacity', 1);
-									}
-								});
-								
-								$('#'+el)[0].style[Modernizr.prefixed('transition')] = 'transform 2s';
-								$('#'+el)[0].style[Modernizr.prefixed('transform')] = 'translateX('+Aldream.metaPositions[el][0]+'em) translateY('+Aldream.metaPositions[el][1]+'em) translateZ('+Aldream.metaPositions[el][2]+'em) rotateX('+Aldream.metaPositions[el][3]+'deg) rotateY('+Aldream.metaPositions[el][4]+'deg) rotateZ('+Aldream.metaPositions[el][5]+'deg)';
-							}
-						});
-					}
+							$('#meta-'+el)[0].style[Modernizr.prefixed('transition')] = 'all 2s';
+							$('#meta-'+el)[0].style[Modernizr.prefixed('transform')] = 'translateX('+Aldream.metaPositions[el][0]+'em) translateY('+Aldream.metaPositions[el][1]+'em) translateZ('+Aldream.metaPositions[el][2]+'em) rotateX('+Aldream.metaPositions[el][3]+'deg) rotateY('+Aldream.metaPositions[el][4]+'deg) rotateZ('+Aldream.metaPositions[el][5]+'deg)';
+						}
+					}, 16);
 					if (Aldream.cache[newPage]) {
 						isContentLoaded = true;
 					}
 					else {
 						$('<div></div>').load(target, function (rep) {
 							isContentLoaded = true;
-							Aldream.cache[newPage] = {meta: {}, body: rep.split('<body>')[1].split('</body>')[0]};
-							if (isDezoomOver == Aldream.metaPositions.length) {
-								for (var elClo in Aldream.metaPositions) {
-									$('#'+elClo).unbind('transitionend webkitTransitionEnd oTransitionEnd otransitionend');
-									if (elClo == Aldream.currentPage) {
-										// We delete the previous content:
-										$('#'+Aldream.currentPage).unbind('transitionend webkitTransitionEnd oTransitionEnd otransitionend');
-										$('#'+Aldream.currentPage).html('');
-									}
-									
-									$('#'+elClo)[0].style[Modernizr.prefixed('transition')] = 'transform 2s';
-									var tr = [];
-									for (var i = 0; i < 6; i++) {
-										tr[i] = Aldream.metaPositions[elClo][i]-Aldream.metaPositions[newPage][i]+Aldream.origOffset[newPage][i];
-									}
-									$('#'+elClo)[0].style[Modernizr.prefixed('transform')] = 'translateX('+Aldream.metaPositions[elClo][0]+'em) translateY('+Aldream.metaPositions[elClo][1]+'em) translateZ('+Aldream.metaPositions[elClo][2]+'em) rotateX('+Aldream.metaPositions[elClo][3]+'deg) rotateY('+Aldream.metaPositions[elClo][4]+'deg) rotateZ('+Aldream.metaPositions[elClo][5]+'deg)';
-									
-									if (elClo == newPage) {
-										$('#'+newPage).html(Aldream.cache[newPage].body);
-										$('#'+ newPage + ' #board').css('opacity', 0);
-										$('#'+ newPage + ' #board')[0].style[Modernizr.prefixed('transition')] = 'opacity 1s 1s';
-										$('#'+ newPage + ' #board').css('opacity', 1);
-									}
-								}
+							Aldream.cache[newPage] = {meta: {}, body: '<section>'+rep.split('<section>')[1].split('<script>')[0]};
+							if (isDezoomOver == Aldream.nbPages) {
+								ZoomIn(newPage);
 							}
 						});
 					}
@@ -115,5 +59,42 @@ $(document).ready(function(){
 				return false;
 			});
 		}
-	});
+		
+		function ZoomIn(newPage) {
+			// We delete the previous content:
+			$('#meta-'+Aldream.currentPage).unbind('transitionend webkitTransitionEnd oTransitionEnd otransitionend');
+			$('#meta-'+Aldream.currentPage).html('');
+			
+			$('#meta-'+newPage).html(Aldream.cache[newPage].body);
+			$('#meta-'+ newPage + ' > section').css('opacity', 0);
+			$('#meta-'+ newPage + ' > section')[0].style[Modernizr.prefixed('transition')] = 'opacity 1s 1s';
+			$('#meta-'+newPage)[0].style[Modernizr.prefixed('transition')] = 'none';
+			setTimeout(function(){
+				$('#meta-'+ newPage + ' > section').css('opacity', 1);
+				
+			for (var elClo in Aldream.metaPositions) {
+
+				$('#meta-'+elClo)[0].style[Modernizr.prefixed('transition')] = 'transform 2s';
+				var tr = [];
+				for (var i = 0; i < 6; i++) {
+					tr[i] = Aldream.metaPositions[elClo][i]-Aldream.metaPositions[newPage][i]+Aldream.origOffset[newPage][i];
+				}
+				tr = 'translateX('+tr[0]+'em) translateY('+(tr[1])+'em) translateZ('+(tr[2])+'em) rotateX('+tr[3]+'deg) rotateY('+tr[4]+'deg) rotateZ('+tr[5]+'deg)';
+				$('#meta-'+elClo)[0].style[Modernizr.prefixed('transform')] = tr;
+			}
+			}, 30);
+			
+			$('#meta-'+ newPage + ' > section').on('transitionend webkitTransitionEnd oTransitionEnd otransitionend', function(e) {
+				e = e.originalEvent.propertyName;
+				$('#meta').remove();
+				$('body').prepend(Aldream.cache[newPage].body);
+				$('body').attr('id', newPage);
+				$('a').each(Navigation);
+				Aldream.currentPage = newPage;
+			});
+			
+			
+		}
+	}
+	$('a').each(Navigation);
 });
